862bc41372710c986544172ccbb6cdb87ac64ed1
Catherine Denis <catherine-denis@outlook.com>
2020-03-29 11:23:02 -0400

    Forms Working and Messages Viewable
    Forms work upon submission. Able to see messages when signing up (does not yet save to db). Sends you back to front page if username doesn't match (doesn't check password yet).

--- messages.php
+++ messages.php
@@ -4,41 +4,65 @@
 	<?php
 		include('mysqli_connect.php');
 		
-	?> 
-	<html lang="en">
-	<meta charset="utf-8"/>
-	<link rel="stylesheet" type="text/css" href="cov.css">
+		$username = $_POST['uname'];
+		
+		if (isset($_POST['uname']) && isset($_POST['login'])) 
+		{
+			
+
+			if (!empty($username)) 
+			{
+				$username_query = mysqli_query($dbc,"SELECT * FROM users
+											   WHERE username = '$username'");
+				$count = mysqli_num_rows($username_query);
+				if($count == 0) {
+					#Username doesn't exist
+					echo "
+						<script>
+						  window.history.back();
+						</script>";
+				}
+				else
+				{
+					#Username already exists
+				}
+			}
+		}
+		elseif (isset($_POST['uname']) && isset($_POST['signup'])) {
+			
+		}
+	echo "<html lang='en'>
+	<meta charset='utf-8'/>
+	<link rel='stylesheet' type='text/css' href='cov.css'>
 </head>
 <body>
-	<div id='usr'></div>
-	<div id="container">
-	
-	<div class="ctrmsg">
-		<?php
-		
+	<div id='topbar'>
+		<p>$username</p>
+		<p><a href='addmessage.html'>Add a Message</a></p>
+	</div><!--topbar-->
+	<div id='container'>
+	<div>";
 			$r = "select id, content, votesup, votesdown from messages";
 			$result = mysqli_query($dbc, $r);
 
 			
 			if ($result->num_rows > 0) {
 				while($row = $result->fetch_assoc()) {
-					$id = $row["id"];
-					echo 
-					"<div class='message'>" . 
-						"<p>" . $row["content"] . "</p>" .
-						"<p>" . $row["votesup"] . 
-							"<button id='vu' onclick='upvote($id)'>👍</button>" . 
-							" " . $row["votesdown"] . 
-							"<button id='vd' onclick='downvote($id)'>👎</button>" . 
-						"</p>" .
-					"</div>";
+					$id = $row['id'];
+					echo "<div class='message'> 
+						<p>" . $row['content'] . "</p>
+						<p> " . $row['votesup'] .
+							"<button id='vu' onclick='upvote($id)'>👍</button>" .
+							 $row['votesdown'] .
+							"<button id='vd' onclick='downvote($id)'>👎</button>
+						</p>
+					</div>";
 				}
 			} else {
-				echo "0 results";
+				echo "<p>No messages</p>";
 			}
 			
-		?>
-		</div>
+		echo"</div>
 	</div><!--container-->
 	<script>
 		function upvote(selector) {
@@ -49,7 +73,6 @@
 		}
 	</script>
 </body>
-</html>
-<?php
+</html>";
 	mysqli_close($dbc);
 ?>
